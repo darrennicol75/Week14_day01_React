b318a183e454cd52dd11e5c93f7eb5156636ec4e
@darrennicol75 <darren.nicol75@gmail.com>
2019-08-05 22:05:12 +0100

    Second commit

---
+++ src/components/MovieItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MovieItem extends Component {
+  render(){
+    return (
+      <li className='movie-item'>
+      <a href={this.props.movie.url}>{this.props.movie.name}</a>
+      </li>
+    );
+  }
+}
+
+export default MovieItem; 
